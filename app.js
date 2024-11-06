3ed5b293b6860306ea40b026eef22f7197a3a64d
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-11-06 15:31:41 +0100

    inserimento foto e titolo da javascript con axios

---
+++ app.js
@@ -0,0 +1,36 @@
+const base_url = 'https://jsonplaceholder.typicode.com/'
+let url_body = 'photos'
+
+const endPoint = base_url + url_body
+
+const albumPhotos = document.getElementById('album-photos')
+console.log(albumPhotos)
+
+axios.get(endPoint, {
+    params: {
+        _limit: 6
+    }
+})
+    .then((res) => {
+        const photos = res.data
+        console.log(photos)
+        appendPhotos(photos, albumPhotos)
+
+    })
+
+function appendPhotos(photos, root) {
+    photos.forEach((photo) => {
+        const { albumId, title, id, url, thumbnailUrl } = photo
+        const cardHtml = `
+        <div class="col-4">
+                        <div class="card">
+                            <img src="${url}" alt="">
+                            <p class="album">${title}</p>
+
+                        </div>
+
+                    </div>
+        `
+        root.innerHTML += cardHtml
+    });
+}
