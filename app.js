94a7190ca643e3e1d0690b91c68376da2eb343bf
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-11-08 11:28:24 +0100

    correzione e completamento esercizio come da lezione

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const albumPhotos = document.getElementById('album-photos')
 const overlayBox = document.getElementById('over')
 //console.log(overlayBox)
 const button = document.getElementById('button')
+const imgOverlay = document.getElementById('picture')
 
 
 axios.get(endPoint, {
@@ -20,16 +21,10 @@ axios.get(endPoint, {
         console.log(photos)
         appendPhotos(photos, albumPhotos)
 
-        let arrayCard = []
-        arrayCard.push(photos)
-        //console.log(arrayCard)
-
-
     })
     .catch((err) => {
         console.log(alert('qualcosa è andato storto'))
     })
-
 
 
 function appendPhotos(photos, root) {
@@ -37,31 +32,48 @@ function appendPhotos(photos, root) {
     photos.forEach((photo) => {
         const { title, url, } = photo
         const cardHtml = `
-         <div class="col-4">
-          <div class="card">
-             <img class="pin" src="./img/pin.svg" alt="">
-             <img class="random-pic" src="${url}" alt="">
-             <p class="album">${title}</p>
+             <div class="col-4">
+              <div class="card">
+                 <img class="pin" src="./img/pin.svg" alt="">
+                 <img class="random-pic" src="${url}" alt="">
+                 <p class="album">${title}</p>
+              </div>
+             </div>
+            `
 
-          </div>
-
-         </div>
-        `
         root.innerHTML += cardHtml
+
     });
+
+
+    const cardsCollection = document.querySelectorAll('.card')
+    // console.log(cardsCollection)
+
+    cardsCollection.forEach((card, i) => {
+        console.log(card)
+
+        card.addEventListener('click', () => {
+
+            overlayBox.classList.remove('display-none')
+
+            const { url } = photos[i]
+            //console.log(url, imgOverlay)
+            imgOverlay.src = url
+            //console.log('ho click sulla card')
+			
+        })
+    })
 }
 
-albumPhotos.addEventListener ('click', function() {
+
+
+
+button.addEventListener('click', function () {
     //console.log('hai cliccato')
-    overlayBox.classList.remove ('display-none')
-    
+    overlayBox.classList.add('display-none')
 })
 
-button.addEventListener ('click', function() {
-    //console.log('hai cliccato')
-    overlayBox.classList.add ('display-none')
-    
-})
+
 
 
 
